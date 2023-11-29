ed8550d08d958053e2dd9ed3ac249c884acd934e
snapp <3gotosnapp@gmail.com>
2023-11-29 20:31:47 +0300

    fix: lab1

--- lab1_13/lab1_13/Program.cs
+++ lab1_13/lab1_13/Program.cs
@@ -6,6 +6,14 @@ namespace lab1_13
     {
         public static void Main(string[] args)
         {
+         //Вариант 13
+         //Описать класс для работы с  одномерным массивом:
+         //- конструктор, заполняющий элементы массива из строки string (числа в строке разделяются двоеточиями);
+         //- конструктор, заполняющий заданное в его параметре количество элементов массива натуральным рядом чисел, знак числа задается случайным образом;
+         //- свойство, доступное только для чтения, для получения количества элементов массива;
+         //- метод, вычисляющий произведение модулей элементов, расположенных до (левее) первого положительного элемента.
+         //    Вывод на экран выполнять только в методе Main класса-клиента. Программа должна адекватно реагировать на ошибки пользователя и различные варианты исходных данных. Все тестовые данные предъявить преподавателю.
+
             var classForWorkingWithAOneDimensionalArray = new OneDimensionArrayWorker(5, ":10:12:14");
             Console.Write(classForWorkingWithAOneDimensionalArray.FindAbsoluteValueMultiplicationOfNotNegativeElementThatIsLeftFirstNotNegativeElement());
             Console.WriteLine(classForWorkingWithAOneDimensionalArray.size);
