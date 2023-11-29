e1a24e243eb0a7640a42817c486db1ce255be921
snapp <3gotosnapp@gmail.com>
2023-11-29 20:31:23 +0300

    lab1 init

---
+++ lab1_13/lab1_13/OneDimensionArrayWorker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace lab1_13
+{
+    public class OneDimensionArrayWorker
+    {
+        private int[] array;
+        public uint size { get; private set; }
+
+        public OneDimensionArrayWorker(uint arrayLength, string massiv)
+        {
+            size = arrayLength;
+            array = new int[arrayLength];
+            string[] separators = { ":"};
+            string[] split = massiv.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                try
+                {
+                    array[i] = int.Parse(split[i]);
+                }
+                catch
+                {
+                    throw new Exception("Data is not valid");
+                }
+            }
+        }
+
+        public int FindAbsoluteValueMultiplicationOfNotNegativeElementThatIsLeftFirstNotNegativeElement() // Нормальное же название, в задании так и написано, отражает суть класс 
+        {
+            var sum = 0;
+
+            foreach (var element in array)
+            {
+                if (element > 0)
+                {
+                    break;
+                   
+                } 
+                sum *= Math.Abs(element);
+            }
+
+            return sum;
+        }
+        
+    }
+}
---
+++ lab1_13/lab1_13/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lab1_13
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var classForWorkingWithAOneDimensionalArray = new OneDimensionArrayWorker(5, ":10:12:14");
+            Console.Write(classForWorkingWithAOneDimensionalArray.FindAbsoluteValueMultiplicationOfNotNegativeElementThatIsLeftFirstNotNegativeElement());
+            Console.WriteLine(classForWorkingWithAOneDimensionalArray.size);
+        }
+    }
+}
